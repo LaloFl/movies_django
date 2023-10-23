98a6dc6dd31aa96aecd646e0540a447d1aa65a32
LaloFl <defsdiego@gmail.com>
2023-10-22 23:12:05 -0700

    models

---
+++ movieProj/movieApp/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# Create your models here.
+class Genero(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+    
+class Estudio(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+    
+class Pelicula(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=50)
+    duracion = models.IntegerField()
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+    estudio = models.ForeignKey(Estudio, on_delete=models.CASCADE)
+    fechaEstreno = models.DateField()
+    enCines = models.BooleanField()
+    poster = models.ImageField(upload_to="peliculas", null=True)
+
+    def __str__(self):
+        return self.titulo
+
+class Actor(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
+
+class Cast(models.Model):
+    id = models.AutoField(primary_key=True)
+    personaje = models.CharField(max_length=50)
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.personaje + " - " + self.actor.nombre + " " + self.actor.apellido
+    
--- movieProj/movieProj/settings.py
+++ movieProj/movieProj/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'movieProj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgres',
+        'NAME': 'movieProj',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres123',
     }
 }
 

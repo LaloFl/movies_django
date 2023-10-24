c5d57651d002f8721b0b148f332e10feaa7dca94
LaloFl <defsdiego@gmail.com>
2023-10-24 14:42:18 -0700

    templates parciales

--- movieProj/movieApp/admin.py
+++ movieProj/movieApp/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-for model in [Genero, Estudio, Pelicula, Actor, Cast]:
+for model in [Genero, Estudio, Pelicula, Actor, Cast, Usuario, Review]:
     admin.site.register(model)
